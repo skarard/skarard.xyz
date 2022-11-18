314c462ee42851abac015c5f66e3df871a5aae4f
skarard <skarard@gmail.com>
2022-11-18 20:30:02 +0000

    Added mastodon verification

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <div className="h-screen flex flex-col justify-center">
-      <div className="text-8xl mx-auto">skarard</div>
+    <div className="bg-slate-900 text-white font-sans h-screen flex flex-col justify-center">
+      <div className="text-9xl mx-auto">skarard</div>
+      <a rel="me" href="https://cryptodon.lol/@skarard"></a>
     </div>
   );
 }
